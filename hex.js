774cc080467cd0aea3401124e532c22574de1fd5
Exequiel Arco <exequielarco23@gmail.com>
2023-10-13 22:26:41 +0800

    Update script.js and hex.js

--- hex.js
+++ hex.js
@@ -4,22 +4,34 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 // Hex format: #f15025 - 6 values.
 
+// ---- ELEMENTS ---- //
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+// ---- END OF  ELEMENTS ---- //
 
-const getRandomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
+// ---- FUNCTIONS ---- //
+// Get random number from 0 - 15
+const getRandomNumber = () => Math.floor(Math.random() * hex.length);
 
+// Get hex color by looping through hex array
 let hexColor;
 const loopHex = () => {
   hexColor = "#";
   for (let i = 0; i < 6; i++) {
-    hexColor += hex[getRandomNumber(0, hex.length - 1)];
+    hexColor += hex[getRandomNumber()];
   }
 };
 
-btn.addEventListener("click", () => {
-  loopHex();
+// Change text and background color
+const changeTxtAndBgc = () => {
   document.body.style.backgroundColor = hexColor;
   color.textContent = hexColor;
+};
+// ---- END OF FUNCTIONS ---- //
+
+// ---- EVENT HANDLER ---- //
+btn.addEventListener("click", () => {
+  loopHex();
+  changeTxtAndBgc();
 });
+// ---- END OF EVENT HANDLER ---- //
--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 "use strict";
 
+// Colors
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
+// ---- ELEMENTS ---- //
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+// ---- END OF ELEMENTS ---- //
 
-const getRandomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
+// ---- FUNCTIONS ---- //
+// Get random number from 0-3
+const getRandomNumber = () => Math.floor(Math.random() * colors.length);
 
-btn.addEventListener("click", () => {
-  // Get random number 0 - 3
-  const randomNumber = getRandomNumber(0, colors.length - 1);
+// Change text and background color
+const changeTxtAndBgc = () => {
+  const randomNumber = getRandomNumber();
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
-});
+};
+// ---- END OF FUNCTIONS ---- //
+
+// ---- EVENT HANDLER ---- //
+btn.addEventListener("click", changeTxtAndBgc);
+// ---- END OF EVENT HANDLER ---- //
